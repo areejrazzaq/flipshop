24d6ef22529b96dd631b1aa4e7c0d0b1e31232f0
areejrazzaq <areejrazzaq00@gmail.com>
2024-11-14 13:00:06 +0500

    updated

--- src/Backend/database/migrations/2023_02_02_081650_create_socials_table.php
+++ src/Backend/database/migrations/2023_02_02_081650_create_socials_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('socials', function (Blueprint $table) {
+            $table->id();
+            $table->string('instagram')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('twitter')->nullable();
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('socials');
     }
 };
--- src/Backend/routes/api.php
+++ src/Backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\User\ProfileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => ['cors', 'json.response', 'api']], function () {
+    Route::post('/register',[AuthController::class, 'register'])->name('auth.register');
+    Route::post('/login',[AuthController::class, 'login'])->name('auth.login');
+    Route::post('/logout',[AuthController::class, 'logout'])->name('auth.logout');
+
+});
+
+Route::middleware(['cors', 'json.response', 'auth:api'])->group(function () {
+    // our routes to be protected will go in here
+    Route::prefix('profile')->group(function(){
+        Route::get('/{id?}',[ProfileController::class,'index'])->name('profile.index');
+        Route::put('/{id}',[ProfileController::class,'update'])->name('profile.update');
+    });
 });
